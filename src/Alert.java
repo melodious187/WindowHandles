e6f024067e3a20445f20c9c5b8a5fd68005049c9
melodious187 <melodious187@fromthemind.com>
2020-06-02 19:47:59 -0700

    Selenium and Web Driver tutorial for June 2nd, 2002

---
+++ src/Alert.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alert {
+
+    public static void main(String[] args)  throws InterruptedException {
+        // launch new chrome browser
+        System.setProperty("webdriver.chrome.driver", "C:/dev/tools/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();  // Launch Chrome browser
+
+        // open website
+        String url = "https://www.testandquiz.com/selenium/testing.html";
+        driver.get(url);
+
+        // Click button which generates alert box
+        driver.findElement(By.xpath("//button[@onclick='generateConfirmBox()']")).click();
+        Thread.sleep(3000);
+
+        // click ok
+        driver.switchTo().alert().accept();
+        Thread.sleep(3000);
+
+        // Click button which generates alert box
+        driver.findElement(By.xpath("//button[@onclick='generateConfirmBox()']")).click();
+        Thread.sleep(3000);
+
+        // click cancel
+        driver.switchTo().alert().dismiss();
+        Thread.sleep(3000);
+
+        driver.quit();
+
+
+    }
+
+}
